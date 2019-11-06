6659e37b1c069a956cb04b7f94d01356d0b6a81e
Dakota Smothers <dsmothers192@gmail.com>
2019-11-06 13:42:25 -0600

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Calculator from './components/Calculator/calculator';
 
 function App() {
   return (
     <div className="App">
-      Hi
+      <Calculator />
     </div>
   );
 }
---
+++ src/components/Calculator/calculator.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './calculator.css';
+
+// Main Calculator component. Contains our other calculator components
+
+
+class Calculator extends Component {
+    render(){
+        return(
+        <div className="Calculator">
+
+        </div>
+        )
+    }
+}
+
+export default Calculator;
