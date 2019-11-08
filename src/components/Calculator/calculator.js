93727facb5502b5b98a06668697ca2149e664ce3
Dakota Smothers <dsmothers192@gmail.com>
2019-11-08 13:46:15 -0600

    finished the calculator

--- src/components/Calculator/calculator.js
+++ src/components/Calculator/calculator.js
@@ -14,8 +14,51 @@ class Calculator extends Component {
         }
     }
     onButtonPress = (event) => {
-        console.log("onButtonPress called");
+        let equation =
+        this.state.equation;
+        const pressedButtonValue =
+        event.target.innerHTML;
+
+        console.log(pressedButtonValue);
+
+        if(pressedButtonValue === 'C'){
+            this.clear();
+            return
+        } else if ((pressedButtonValue >= '0' && pressedButtonValue <= '9') || pressedButtonValue === '.'){
+            /* If the user has pressed a button with a value between 0 and 9 or the period, add that value to the equation */
+            equation += pressedButtonValue;
+        } else if (['+', '-', '*', '/', '%'].indexOf(pressedButtonValue) !== -1){
+            equation += ' ' + pressedButtonValue + ' ';
+        } else if(pressedButtonValue === '='){
+            /* If the user hits the enter button, calculate stuff */
+            /* Try catch blocks = try to run the code in the try block. If it fails and throws an error, deal with it in the catch block */
+            try {
+                const evalResult  = eval(equation);
+                const result = Number.isInteger(evalResult) ? 
+                evalResult : evalResult.toFixed(2);
+                this.setState({ result });
+            } catch(error){
+                /* If an error occurs, print it to the console and alert the user */
+                console.log(error);
+                alert('An error occured. YOU SUCK! WHY ARE YOU EVEN DOING THIS? YOU WILL NEVER BE SUCCESSFUL.');
+
+            }
+
+        } else {
+            /* If the user has pressed the back arrow, remove the last character from the equation string */
+            equation = equation.trim();
+            equation = equation.substr(0, equation.length - 1);
+        }
+
+        this.setState({equation});
     }
+
+    clear(){
+        this.setState({ equation: '', 
+        result: 0
+    })
+}
+
 
     render() {
         return (
